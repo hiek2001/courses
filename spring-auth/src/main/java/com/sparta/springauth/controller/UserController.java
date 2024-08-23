5cd01deb959a3b4125bb52ee5286cfdde7cd0a48
hiek2001 <hiek2001@naver.com>
2024-08-23 18:14:38 +0900

    validation 예외처리

--- spring-auth/src/main/java/com/sparta/springauth/controller/UserController.java
+++ spring-auth/src/main/java/com/sparta/springauth/controller/UserController.java
@@ -5,11 +5,18 @@ import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.entity.User;
 import com.sparta.springauth.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequestMapping("/api")
 public class UserController {
@@ -32,7 +39,15 @@ public class UserController {
     }
 
     @PostMapping("/user/signup")
-    public String signup(SignupRequestDto requestDto) { // @ModelAttribute는 생략 가능
+    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) { // @ModelAttribute는 생략 가능
+        // validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors(); // 오류가 난 field들을 하나씩 가져옴
+        if(fieldErrors.size() > 0) {
+            for(FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "redirect:/api/user/signup";
+        }
         userService.signup(requestDto);
 
         return "redirect:/api/user/login-page";
