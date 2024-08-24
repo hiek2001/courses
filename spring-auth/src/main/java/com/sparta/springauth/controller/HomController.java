b824c59332b579b880f27348d25f9336584ca1c5
hiek2001 <hiek2001@naver.com>
2024-08-24 17:17:22 +0900

    회원가입 구현

---
+++ spring-auth/src/main/java/com/sparta/springauth/controller/HomController.java
@@ -0,0 +1,16 @@
+package com.sparta.springauth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomController {
+
+    // 메인 페이지로 이동
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("username", "username");
+        return "index";
+    }
+}
