22654b80874756dbf50ac397f1c0e712938c1e72
hiek2001 <hiek2001@naver.com>
2024-08-24 17:31:23 +0900

    로그인 구현 : JWT

--- spring-auth/src/main/java/com/sparta/springauth/controller/UserController.java
+++ spring-auth/src/main/java/com/sparta/springauth/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.sparta.springauth.controller;
 
+import com.sparta.springauth.dto.LoginRequestDto;
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.entity.User;
 import com.sparta.springauth.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,5 +36,16 @@ public class UserController {
         userService.signup(requestDto);
 
         return "redirect:/api/user/login-page";
+    }
+
+    @PostMapping("/user/login")
+    public String login(LoginRequestDto requestDto, HttpServletResponse res) {
+        try {
+            userService.login(requestDto, res);
+        } catch (Exception e) { // 로그인 페이지로 넘겨줌
+            return "redirect:/api/user/login-page?error";
+        }
+
+        return "redirect:/";
     }
 }
