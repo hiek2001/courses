834e442112d093c85a4a056d4a77030a4d6b8c00
hiek2001 <hiek2001@naver.com>
2024-08-27 16:46:13 +0900

    중간 테이블 Order 테이블 추가

--- jpa-advance/src/main/java/com/sparta/jpaadvance/entity/Order.java
+++ jpa-advance/src/main/java/com/sparta/jpaadvance/entity/Order.java
@@ -3,11 +3,16 @@ package com.sparta.jpaadvance.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "orders")
+@EntityListeners(AuditingEntityListener.class)
 public class Order { // 직접 생성했으므로, Order가 Food, User의 외래키의 주인
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +25,9 @@ public class Order { // 직접 생성했으므로, Order가 Food, User의 외래
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    // JPA auditing
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime orderDate;
 }
