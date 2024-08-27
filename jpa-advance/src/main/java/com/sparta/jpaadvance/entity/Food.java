25602b0b27b5ed0250325d08d42d4f0c331186bb
hiek2001 <hiek2001@naver.com>
2024-08-27 16:39:35 +0900

    중간 테이블 Order 테스트

--- jpa-advance/src/main/java/com/sparta/jpaadvance/entity/Food.java
+++ jpa-advance/src/main/java/com/sparta/jpaadvance/entity/Food.java
@@ -18,25 +18,7 @@ public class Food { // 외래키의 주인
     private String name;
     private double price;
 
-    // N 대 M
-    @ManyToMany
-    @JoinTable(name = "orders", // 중간 테이블 생성
-            joinColumns = @JoinColumn(name = "food_id"), // 현재 위치인 Food Entity 에서 중간 테이블로 조인할 컬럼 설정
-            inverseJoinColumns = @JoinColumn(name = "user_id")) // 반대 위치인 User Entity 에서 중간 테이블로 조인할 컬럼 설정
-    private List<User> userList = new ArrayList<>();
+    @OneToMany(mappedBy = "food")
+    private List<Order> orderList = new ArrayList<>();
 
-    // 양방향 설정
-    public void addUserList(User user) {
-        this.userList.add(user); // 외래 키(연관 관계) 설정
-        user.getFoodList().add(this);
-    }
-
-    // N 대 1
-//    @ManyToOne
-//    @JoinColumn(name = "user_id")
-//    private User user;
-    // 1 대 1
-//    @OneToOne
-//    @JoinColumn(name = "user_id")
-//    private User user;
 }
--- jpa-advance/src/main/java/com/sparta/jpaadvance/entity/User.java
+++ jpa-advance/src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -17,34 +17,7 @@ public class User {
     private Long id;
     private String name;
 
-    // N 대 M
-    @ManyToMany(mappedBy = "userList")
-    private List<Food> foodList = new ArrayList<>();
-
-    // 양방향 설정
-    public void addFoodList(Food food) {
-        this.foodList.add(food);
-        food.getUserList().add(this); // 외래 키(연관 관계) 설정
-    }
-
-    // N 대 1
-//    @OneToMany(mappedBy = "user")
-//    private List<Food> foodList = new ArrayList<>(); // db에는 적용되지 않고, entity를 참조하기 위해 존재하는 것
-//
-//    public void addFoodList(Food food) {
-//        this.foodList.add(food);
-//        food.setUser(this); // 외래키(연관 관계) 설정
-//    }
-
-
-    // 1 대 1
-//    @OneToOne(mappedBy = "user")
-//    private Food food;
-//
-//    public void addFood(Food food) {
-//        this.food = food;
-//        food.setUser(this);
-//    }
-
+    @OneToMany(mappedBy = "user")
+    private List<Order> orderList = new ArrayList<>();
 
 }
