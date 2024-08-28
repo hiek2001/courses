325a4ea59205041c9b7c23df2a647950e1813aff
hiek2001 <hiek2001@naver.com>
2024-08-28 13:28:54 +0900

    관심상품 등록 API

---
+++ myselectshop/src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -0,0 +1,22 @@
+package com.sparta.myselectshop.service;
+
+import com.sparta.myselectshop.dto.ProductRequestDto;
+import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.repository.ProductRespository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRespository productRespository;
+
+    public ProductResponseDto createProduct(ProductRequestDto requestDto) {
+        Product product = productRespository.save(new Product(requestDto));
+        return new ProductResponseDto(product);
+    }
+}
