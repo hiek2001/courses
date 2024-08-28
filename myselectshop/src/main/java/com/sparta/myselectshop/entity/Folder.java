6ba4575857d4af618ff55ae62621969e2ef460a3
hiek2001 <hiek2001@naver.com>
2024-08-28 17:06:13 +0900

    상품 폴더 설계

---
+++ myselectshop/src/main/java/com/sparta/myselectshop/entity/Folder.java
@@ -0,0 +1,30 @@
+package com.sparta.myselectshop.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name="folder")
+public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY) // user 정보를 항상 가져올 필요가 없기 때문에 LAZY로 설정
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Folder(String name, User user) {
+        this.name = name;
+        this.user = user;
+    }
+}
